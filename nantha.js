7d64689de0fe3b51438cf7815b8288ab0d43d425
NANTHA KUMAR . R <nantha19042003@gmail.com>
2025-02-03 20:56:11 +0530

    Add files via upload

---
+++ nantha.js
@@ -0,0 +1,24 @@
+
+
+
+// for(i=1;i<=100;i++ ) 
+// {
+//   console.log(i + "   " +"NANTHA KUMAR")
+// }
+// {
+// let item = "summer";
+
+
+//     if (item == "summer"
+//     )
+//     {
+//         console.log("enjoy");
+        
+//     }
+//     else
+//     {
+//         console.log("so sad");
+        
+//     }
+
+// }
